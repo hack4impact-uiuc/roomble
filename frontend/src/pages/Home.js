03975b81ed97a1372983e8c410f79e857c42dd74
Alice Fang <alicesf2@illinois.edu>
2020-11-19 19:46:58 -0600

    update styling of homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,20 @@
 import '../styles/Home.css';
+import {Link} from "react-router-dom"
 import { Button } from 'semantic-ui-react'
 
 function Home() {
   return (
     <div class="homepage">
-      <h1>Roomble</h1>
-      <h2>Get ready to ROOM-ble!!!</h2>
-      <Button size='massive'>Log In</Button>
+      <h1 class="hp-header" >Roomble</h1>
+      <h2 class="hp-subheader">Get ready to ROOM-ble!!!</h2>
+      <Button size='massive'>
+        <Link to="/login">Log In</Link>
+      </Button>
       <br />
-      <br/>
-      <Button size='massive'>Sign Up</Button>
+      <br />
+      <Button size='massive'>
+        <Link to="/createProfile">Sign Up</Link>
+      </Button>
     </div>
   );
 }
