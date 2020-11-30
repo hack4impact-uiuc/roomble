31b4eb3bae371b07bd39c27e947ade2960b8013e
Vasudha Chalasani <40867028+vasuchal@users.noreply.github.com>
2020-11-30 16:54:24 -0600

    utils

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,27 @@
+export async function makePost(name, school, year, age, gender, major, phoneNumber, fbUsername, igUsername, scUsername, email, housingType, numRoommates, shortDesc, longDesc, quizAnswers) {
+    
+    const response = await fetch("http://localhost:5000/createProfile", {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            "name": name,
+            "school": school,
+            "year": year,
+            "age": age,
+            "gender": gender,
+            "major": major,
+            "phoneNumber": phoneNumber,
+            "fbUsername" : fbUsername,
+            "igUsername" : igUsername,
+            "scUsername" : scUsername,
+            "email" : email,
+            "housingType" : housingType,
+            "numRoommates" : numRoommates,
+            "shortDesc" : shortDesc,
+            "longDesc" : longDesc,
+            "quizAnswers" : quizAnswers
+        })
+    });
+}
