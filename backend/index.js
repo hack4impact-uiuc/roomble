adf8f9a652ee5a99056607e83ea7c4bef1044351
riyajain59 <riyaj3@illinois.edu>
2020-12-01 22:26:39 -0500

    addes files

--- backend/index.js
+++ backend/index.js
@@ -21,19 +21,6 @@ var db = mongoose.connection;
 app.get('/loginpage', async (req, res) => {
   res.send('Hello World')
 });
-
-app.post('/submitQuiz', async (req, res) => {
-  const quizAnswers = req.body["quizAnswers"];
- 
-  const filter = {_id:  new mongoose.Types.ObjectId("asdfadfasdf");};
-  const update = { quizAnswers : quizAnswers};
-  let doc = await Profile.findOneAndUpdate(filter, update);
-
-  //get user's id
-  //find that user's profile
-  //update that user's profile with those quiz answers
-  //respond with success
-});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
