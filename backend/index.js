7922df6d4e1989f53de7a8efa2de6780a7bf897c
Brandon Wang <sourbrandon@gmail.com>
2020-12-01 22:48:58 -0500

    add routes to login

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require("mongoose");
+const session = require('express-session');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+
+const authRoutes = require('./routes/auth.js');
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: '\'' + process.env.SESSION_SECRET + '\'',
+    resave: true,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var User = require('./models/User')
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
@@ -18,9 +40,20 @@ var db = mongoose.connection;
 // const Likes = require("./models/Likes.js");
 // const User = require("./models/User.js");
 
-app.get('/loginpage', async (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello World')
 });
+
+app.use('/auth/', authRoutes)
+
+app.get('/getProfile', async (req, res) => {
+    if (req.isAuthenticated()) {
+        req.user.username
+    } else {
+        res.send('Not logged in!')
+    }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
